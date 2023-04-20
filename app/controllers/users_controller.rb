fd0933c5abf131a25b0a3d60a27fc28656e8276d
Flapjack <nicholaus.brabant@gmail.com>
2023-04-19 18:59:40 -0700

    :bread: Add toast component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_path, notice: 'User successfully created.'
+      redirect_to users_path, notice: 'User created.'
     else
       render :new, status: :unprocessable_entity
     end
